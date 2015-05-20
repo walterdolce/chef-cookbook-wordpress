89a71b88f5f591de4ecccf29120aa3a32d05ad00
Walter Dolce <walterdolce@gmail.com>
2015-05-20 20:12:08 +0100

    Add IIS package type suggestion and versions list link

--- attributes/downloader.rb
+++ attributes/downloader.rb
@@ -1,4 +1,4 @@
 default['wordpress']['downloader']['protocol'] = 'https'
 default['wordpress']['downloader']['source'] = 'wordpress.org'
-default['wordpress']['downloader']['package_version'] = 'latest'
-default['wordpress']['downloader']['package_type'] = 'zip' # zip | gzip
+default['wordpress']['downloader']['package_version'] = 'latest' # See https://wordpress.org/download/release-archive/ for all available versions
+default['wordpress']['downloader']['package_type'] = 'zip' # zip | gzip | IIS
