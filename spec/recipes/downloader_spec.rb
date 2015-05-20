7ba043531edcb805be4ee0f68e0ac43bd4e79f96
Walter Dolce <walterdolce@gmail.com>
2015-05-20 23:04:00 +0100

    Contextualyze default attributes specs

--- spec/recipes/downloader_spec.rb
+++ spec/recipes/downloader_spec.rb
@@ -6,20 +6,25 @@ describe 'chef-cookbook-wordpress::downloader' do
     ChefSpec::Runner.new({cookbook_path: '../'}).converge(described_recipe)
   }
 
-  it 'uses HTTPS as default protocol to download Wordpress' do
-    expect(chef_run.node['wordpress']['downloader']['protocol']).to eq('https')
+  context 'default attributes' do
+
+    it 'uses HTTPS as default protocol to download Wordpress' do
+      expect(chef_run.node['wordpress']['downloader']['protocol']).to eq('https')
+    end
+
+    it 'uses the default Wordpress site URL as source' do
+      expect(chef_run.node['wordpress']['downloader']['source']).to eq('wordpress.org')
+    end
+
+    it 'uses the latest Wordpress version as default package to download' do
+      expect(chef_run.node['wordpress']['downloader']['package_version']).to eq('latest')
+    end
+
+    it 'uses zip as default package type to get' do
+      expect(chef_run.node['wordpress']['downloader']['package_type']).to eq('zip')
+    end
+
   end
 
-  it 'uses the default Wordpress site URL as source' do
-    expect(chef_run.node['wordpress']['downloader']['source']).to eq('wordpress.org')
-  end
-
-  it 'uses the latest Wordpress version as default package to download' do
-    expect(chef_run.node['wordpress']['downloader']['package_version']).to eq('latest')
-  end
-
-  it 'uses zip as default package type to get' do
-    expect(chef_run.node['wordpress']['downloader']['package_type']).to eq('zip')
-  end
 
 end
