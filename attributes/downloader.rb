1ff1420835296e24f96c2cc9e84ec46dae44077e
Walter Dolce <walterdolce@gmail.com>
2015-05-22 23:22:17 +0100

    Add explanatory comment in downloader attribute file

--- attributes/downloader.rb
+++ attributes/downloader.rb
@@ -1,6 +1,6 @@
-default['wordpress']['downloader']['protocol'] = 'https'
+default['wordpress']['downloader']['protocol'] = 'https' # https | http | ftp | file
 default['wordpress']['downloader']['source'] = 'wordpress.org'
 default['wordpress']['downloader']['package_version'] = 'latest' # See https://wordpress.org/download/release-archive/ for all available versions
-default['wordpress']['downloader']['package_type'] = 'zip' # zip | gzip | IIS
+default['wordpress']['downloader']['package_type'] = 'zip' # zip | gzip | iis
 default['wordpress']['downloader']['destination'] = './'
 default['wordpress']['downloader']['destination_filename'] = 'wordpress-latest'
