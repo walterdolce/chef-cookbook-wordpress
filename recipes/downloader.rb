948de32c14179f7b32038f3385642a989b97e580
Walter Dolce <walterdolce@gmail.com>
2015-05-30 11:20:26 +0100

    Refactor downloader

--- recipes/downloader.rb
+++ recipes/downloader.rb
@@ -1,21 +1,21 @@
-node_attribute = node['wordpress']['downloader']
+downloader = node['wordpress']['downloader']
 
-destination_uri = node_attribute['destination']
-filename = "#{node_attribute['destination_filename']}.#{node_attribute['package_type']}"
+destination_uri = downloader['destination']
+filename = "#{downloader['destination_filename']}.#{downloader['package_type']}"
 
 directory destination_uri do
-  user node_attribute['destination_dir_user']
-  group node_attribute['destination_dir_group']
+  user downloader['destination_dir_user']
+  group downloader['destination_dir_group']
   recursive true
   action :create
 end
 
-source_uri = node_attribute['protocol'] + '://' + node_attribute['source'] + '/'
-source_uri = source_uri + node_attribute['package_version'] + '.' + node_attribute['package_type']
+source_uri = downloader['protocol'] + '://' + downloader['source'] + '/'
+source_uri = source_uri + downloader['package_version'] + '.' + downloader['package_type']
 
 remote_file File.join(destination_uri, filename) do
   source source_uri
-  user node_attribute['destination_file_user']
-  group node_attribute['destination_file_group']
+  user downloader['destination_file_user']
+  group downloader['destination_file_group']
   action :create
 end
