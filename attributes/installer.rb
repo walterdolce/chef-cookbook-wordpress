996eb55b94f5785266005b117c0332876b025b57
Walter Dolce <walterdolce@gmail.com>
2015-06-07 14:42:35 +0100

    Add initial wp-config template attributes

--- attributes/installer.rb
+++ attributes/installer.rb
@@ -1,2 +1,19 @@
 default['wordpress']['installer']['extraction_destination'] = './'
 default['wordpress']['installer']['wp_config_file_mode'] = 0640
+
+default['wordpress']['installer']['wp_config']['db_name'] = ''
+default['wordpress']['installer']['wp_config']['db_user'] = ''
+default['wordpress']['installer']['wp_config']['db_password'] = ''
+default['wordpress']['installer']['wp_config']['db_host'] = 'localhost'
+default['wordpress']['installer']['wp_config']['db_charset'] = 'utf8'
+default['wordpress']['installer']['wp_config']['db_collate'] = ''
+default['wordpress']['installer']['wp_config']['auth_key'] = ''
+default['wordpress']['installer']['wp_config']['secure_auth_key'] = ''
+default['wordpress']['installer']['wp_config']['logged_in_key'] = ''
+default['wordpress']['installer']['wp_config']['nonce_key'] = ''
+default['wordpress']['installer']['wp_config']['auth_salt'] = ''
+default['wordpress']['installer']['wp_config']['secure_auth_salt'] = ''
+default['wordpress']['installer']['wp_config']['logged_in_salt'] = ''
+default['wordpress']['installer']['wp_config']['nonce_salt'] = ''
+default['wordpress']['installer']['wp_config']['table_prefix'] = 'wp_'
+default['wordpress']['installer']['wp_config']['wp_debug'] = false
