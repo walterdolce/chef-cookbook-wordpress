d2e8be5ac4508a4be475421aa65f4288b935ab01
Walter Dolce <walterdolce@gmail.com>
2015-06-25 00:10:14 +0100

    Add integration test for directory resource

--- test/integration/downloader/serverspec/downloader_spec.rb
+++ test/integration/downloader/serverspec/downloader_spec.rb
@@ -2,7 +2,13 @@ require 'serverspec'
 
 set :backend, :exec
 
-describe file('/wordpress-latest.zip') do
+describe file('/mydownloadfolder') do
+  it { should be_a_directory }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+end
+
+describe file('/mydownloadfolder/wordpress-latest.zip') do
   it { should be_file }
   it { should be_owned_by 'root' }
   it { should be_grouped_into 'root' }
